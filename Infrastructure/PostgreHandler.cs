11b8a11bd0e0e493a00fcfc3924cc6a5417c3b61
mcdnmd <kpoltoradnev@gmail.com>
2020-11-16 22:15:57 +0500

    #1 Infrastructure. Init commit

--- Infrastructure/PostgreHandler.cs
+++ Infrastructure/PostgreHandler.cs
@@ -1,44 +1,44 @@
-using System.Collections.Generic;
+using System;
 using Domain;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
-
-namespace Infrastructure{
+namespace Infrastructure
+{
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-         
+
         public ApplicationContext()
         {
             Database.EnsureCreated();
         }
-         
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("");
         }
     }
+
     public class PostgreHandler<T> : IDataBase<T>
     {
         public void Add(T obj)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void Remove(T obj)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void Select(string key)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void Insert(T obj)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
