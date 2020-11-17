cfd6d08b4534bcb286511c57f5c9da700eaaafab
mcdnmd <kpoltoradnev@gmail.com>
2020-11-17 11:21:17 +0500

    #2 App. Add fields in RemoveUser.cs

--- App/UserRequest/RemoveUser.cs
+++ App/UserRequest/RemoveUser.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Collections.Generic;
+using App.BotTask;
+using Domain;
+using Infrastructure;
+
 namespace App.UserRequest
 {
-    public class RemoveUser
+    public class RemoveUser : IUserRequest
     {
+        public Action Task { get; }
+
+        public RemoveUser(string userId, Dictionary<string, string> parameters, IDataBase<IDataBaseElement> dataBase)
+        {
+            Task = CreateTask(userId, parameters, dataBase);
+        }
         
+        private static Action CreateTask(string userId, Dictionary<string, string> parameters,
+            IDataBase<IDataBaseElement> dataBase)
+        {
+            return delegate
+            {
+                var user = new User {Id = userId, Token = parameters["api_token"]};
+                dataBase.Remove(user);
+            };
+        }
     }
 }
