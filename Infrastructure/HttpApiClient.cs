187b8fb134ba86fa7e1b6234a5d310b48400281d
Гриша <aydartsyang@gmail.com>
2020-11-20 17:07:21 +0500

    #1.1 Added HttpClient with Get request

---
+++ Infrastructure/HttpApiClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class HttpApiClient
+    {
+        private readonly HttpClient client;
+        private readonly string URL;
+
+        public HttpApiClient(string token)
+        {
+            client = new HttpClient();
+            URL = $"https://cloud.iexapis.com/stable/stock/intc/quote/?token={token}";
+        }
+
+        public async Task<string> GET()
+        {
+            var response = await client.GetAsync(URL);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public string GetInfo()
+        {
+            try
+            {
+                return GET().Result;
+            }
+            catch (AggregateException e)
+            {
+                return e.GetBaseException().Message;
+            }
+        }
+    }
+}
