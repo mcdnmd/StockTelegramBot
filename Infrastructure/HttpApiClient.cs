c9075a0bc4f931aa7d28c04959cdd24644d6a598
Aydartsyan_Grisha <57267232+AydartsyanGrisha@users.noreply.github.com>
2020-11-24 09:14:45 +0500

    Infrastructure (#1)
    * #1.1 Added HttpClient with Get request
    
    * #1.2 Changed some fields names and modificators

---
+++ Infrastructure/HttpApiClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class HttpApiClient
+    {
+        private readonly HttpClient client;
+        private readonly string url;
+
+        public HttpApiClient(string token)
+        {
+            client = new HttpClient();
+            url = $"https://cloud.iexapis.com/stable/stock/intc/quote/?token={token}";
+        }
+
+        private async Task<string> Get()
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public string GetInfo()
+        {
+            try
+            {
+                return Get().Result;
+            }
+            catch (AggregateException e)
+            {
+                return e.GetBaseException().Message;
+            }
+        }
+    }
+}
