fc8da76ba62ecada09b20a78be40459608232c47
mcdnmd <kpoltoradnev@gmail.com>
2020-11-25 15:29:36 +0500

    #1 View. Add TelegramHandler

---
+++ View/TelegramHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using App.BotTask;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace View
+{
+    public class TelegramHandler
+    {
+        private ITelegramBotClient botClient;
+
+        public Action<UserRequest> OnMessage;
+
+        public TelegramHandler(ITelegramBotClient botClient)
+        {
+            this.botClient = botClient;
+        }
+
+        public void Initialize()
+        {
+            botClient.OnMessage += BotOnMessage;
+            botClient.StartReceiving();
+        }
+
+        private async void BotOnMessage(object sender, MessageEventArgs e)
+        {
+            var userId = e.Message.Chat.Id;
+            UserRequestType userRequestType;
+            switch (e.Message.Text)
+            {
+                case null:
+                    return;
+                case "/register":
+                    userRequestType = UserRequestType.Register;
+                    break;
+                case "/addsymbol":
+                    userRequestType = UserRequestType.SubscribeForSymbol;
+                    break;
+                case "/removesymbol":
+                    userRequestType = UserRequestType.UnSubscribeForSymbol;
+                    break;
+                default:
+                    if (e.Message.Text.StartsWith('/'))
+                    {
+                        SendReply();
+                        return;
+                    }
+                    else
+                        userRequestType = UserRequestType.InputRawData;
+                    break;
+            }
+            OnMessage(new UserRequest(userId, userRequestType));
+        }
+
+        private async void SendReply()
+        {
+            
+        }
+    }
+}
