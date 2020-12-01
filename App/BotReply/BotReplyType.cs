8481f153b84940c93778b47190fee6e012c88f5d
mcdnmd <kpoltoradnev@gmail.com>
2020-12-01 09:15:16 +0500

    #5.1 App. Changed Enum BotReplyType.cs

--- App/BotReply/BotReplyType.cs
+++ App/BotReply/BotReplyType.cs
@@ -3,6 +3,9 @@ namespace App
     public enum BotReplyType
     {
         UnknownCommand,
+        RequestForChoseParser,
+        RequestForEnterParserPublicToken,
+        RequestForEnterSymbol,
         SingleSymbolInfo,
         MultipleSymbolInfo,
     }
