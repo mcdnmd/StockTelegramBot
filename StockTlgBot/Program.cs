11c310a42925f65ae4ae30a8dc35d0857de421e1
mcdnmd <kpoltoradnev@gmail.com>
2020-12-04 12:08:37 +0500

    #4.1 StockTlgBot. Changes Program.cs

--- StockTlgBot/Program.cs
+++ StockTlgBot/Program.cs
@@ -21,6 +21,9 @@ namespace StockTlgBot
         static void Main(string[] args)
         {
             botClient = new TelegramBotClient("1412654956:AAFoRDvW1H_uG2VB9id2lPBrkYS1fLDhJ7E");
+            
+            InitDb();
+            
             botLogic = new BotLogic(new PostgreHandler());
             telegramHandler = new TelegramHandler(botClient);
             
