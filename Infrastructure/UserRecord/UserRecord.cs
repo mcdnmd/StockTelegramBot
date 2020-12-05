9645da7f754957210c6109ba06fbcc76929ff7b6
mcdnmd <kpoltoradnev@gmail.com>
2020-12-05 13:50:59 +0500

    #5 Infrastructure. Change fields in UserRecord

--- Infrastructure/UserRecord/UserRecord.cs
+++ Infrastructure/UserRecord/UserRecord.cs
@@ -4,10 +4,10 @@ namespace Infrastructure
 {
     public class UserRecord
     {
-        public long Id;
-        public ChatStatus ChatStatus;
-        public List<string> Subscriptons;
-        public ParserName ParserName;
-        public string ParserToken;
+        public long Id { get; set; }
+        public ChatStatus ChatStatus { get; set; }
+        public List<string> Subscriptons { get; set; }
+        public ParserName ParserName { get; set; }
+        public string ParserToken { get; set; }
     }
 }
